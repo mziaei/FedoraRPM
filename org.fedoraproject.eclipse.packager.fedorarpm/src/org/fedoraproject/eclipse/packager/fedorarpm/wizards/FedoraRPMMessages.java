bac3459f9b6e083abbe8ed19beb9814e5057bfaa
Minoo Ziaei <mziaei@redhat.com>
2011-07-04 16:55:39 -0400

    Adding controls

---
+++ org.fedoraproject.eclipse.packager.fedorarpm/src/org/fedoraproject/eclipse/packager/fedorarpm/wizards/FedoraRPMMessages.java
@@ -0,0 +1,39 @@
+package org.fedoraproject.eclipse.packager.fedorarpm.wizards;
+
+import org.eclipse.osgi.util.NLS;
+
+public class FedoraRPMMessages extends NLS {
+	private static final String BUNDLE_NAME = "org.fedoraproject.eclipse.packager.fedorarpm.wizards.messages"; //$NON-NLS-1$
+	public static String FedoraRPMProject_0;
+	public static String FedoraRPMProject_1;
+	public static String FedoraRPMProject_2;
+	public static String FedoraRPMProject_3;
+	public static String FedoraRPMProject_4;
+	public static String FedoraRPMProject_5;
+	public static String FedoraRPMProjectPageOne_fGroup_text;
+	public static String FedoraRPMProjectPageOne_btnRadioButton_text;
+	public static String FedoraRPMProjectPageOne_btnNewMaintainer_text;
+	public static String FedoraRPMProjectPageOne_btnRadioButton_1_text;
+	public static String FedoraRPMProjectPageOne_btnCheckButton_text;
+	public static String FedoraRPMProjectPageOne_grpA_text;
+	public static String FedoraRPMProjectPageOne_btnNewButton_text;
+	public static String FedoraRPMProjectPageOne_btnRadioButton_text_1;
+	public static String FedoraRPMProjectPageOne_btnRadioButton_1_text_1;
+	public static String FedoraRPMProjectPageOne_grpAccountSetup_text;
+	public static String FedoraRPMProjectPageOne_btnCheckButton_text_1;
+	public static String FedoraRPMProjectPageOne_lblNewLabel_text;
+	public static String FedoraRPMProjectPageOne_lblNewLabel_1_text;
+	public static String FedoraRPMProjectPageOne_lblNewLabel_text_1;
+	public static String FedoraRPMProjectPageOne_lblNewLabel_1_text_1;
+	public static String FedoraRPMProjectPageOne_btnCheckButton_text_2;
+	public static String FedoraRPMProjectPageOne_btnCheckButton_1_text;
+	public static String FedoraRPMProjectPageOne_btnCheckButton_2_text;
+	
+	static {
+		// initialize resource bundle
+		NLS.initializeMessages(BUNDLE_NAME, FedoraRPMMessages.class);
+	}
+
+	private FedoraRPMMessages() {
+	}
+}
--- org.fedoraproject.eclipse.packager.fedorarpm/src/org/fedoraproject/eclipse/packager/fedorarpm/wizards/FedoraRPMProjectWizard.java
+++ org.fedoraproject.eclipse.packager.fedorarpm/src/org/fedoraproject/eclipse/packager/fedorarpm/wizards/FedoraRPMProjectWizard.java
@@ -1,11 +1,20 @@
 package org.fedoraproject.eclipse.packager.fedorarpm.wizards;
 
+import java.lang.reflect.InvocationTargetException;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.wizard.Wizard;
 import org.eclipse.ui.INewWizard;
 import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.actions.WorkspaceModifyOperation;
+import org.eclipse.linuxtools.rpm.core.RPMProjectCreator;
 
 public class FedoraRPMProjectWizard extends Wizard implements INewWizard {
+	private static final String PAGE_ONE = "PageOne"; 
+	
+	private FedoraRPMProjectPageOne pageOne;
 
 	public FedoraRPMProjectWizard() {
 		// TODO Auto-generated constructor stub
@@ -16,11 +25,38 @@ public class FedoraRPMProjectWizard extends Wizard implements INewWizard {
 		// TODO Auto-generated method stub
 
 	}
+	
+	/* (non-Javadoc)
+	 * @see org.eclipse.jface.wizard.Wizard#addPages()
+	 */
+	public void addPages() {
+		super.addPages();
+		pageOne = new FedoraRPMProjectPageOne(PAGE_ONE);
+		addPage(pageOne);	
+	}
 
 	@Override
 	public boolean performFinish() {
-		// TODO Auto-generated method stub
-		return false;
+		try {
+			WorkspaceModifyOperation op = new WorkspaceModifyOperation() {
+				@Override
+				protected void execute(IProgressMonitor monitor) {
+					createProject(monitor != null ? monitor
+							: new NullProgressMonitor());
+				}
+			};
+			getContainer().run(false, true, op);
+		} catch (InvocationTargetException x) {
+			return false;
+		} catch (InterruptedException x) {
+			return false;
+		}
+		return true;
+	}
+	
+	protected void createProject(IProgressMonitor monitor) {
+		RPMProjectCreator rpmProjectCreator = new RPMProjectCreator();
+		rpmProjectCreator.create(pageOne.getProjectName(), pageOne.getLocationPath(), monitor);
 	}
 
 }
