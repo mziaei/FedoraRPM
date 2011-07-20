a313d61d42d7607ec245fddb22db086ed3ba3282
Minoo Ziaei <mziaei@redhat.com>
2011-07-20 17:08:45 -0400

    added jgit init stuff

--- org.fedoraproject.eclipse.packager.fedorarpm.ui/src/org/fedoraproject/eclipse/packager/fedorarpm/core/FedoraRPMProjectCreator.java
+++ org.fedoraproject.eclipse.packager.fedorarpm.ui/src/org/fedoraproject/eclipse/packager/fedorarpm/core/FedoraRPMProjectCreator.java
@@ -1,7 +1,11 @@
 package org.fedoraproject.eclipse.packager.fedorarpm.core;
 
 import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
@@ -12,12 +16,18 @@ import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.Platform;
+import org.eclipse.jgit.api.InitCommand;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.util.FileUtils;
 
 public class FedoraRPMProjectCreator {
 	private IWorkspaceRoot root;
 	private IProject project;
 	private IProjectDescription description;
-
+	private static int testCount;
+	private final File trash = new File(new File("target"), "trash");
+	private final List<Repository> toClose = new ArrayList<Repository>();
+	
 	/**
 	 * Creates a project with the given name in the given location.
 	 * @param projectName The name of the project.
@@ -45,18 +55,50 @@ public class FedoraRPMProjectCreator {
 
 		} catch (CoreException e) {
 			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
 
 	/**
 	 * Initialize a local git repository in project location
+	 * @throws IOException 
 	 */
-	private void createLocalGitRepo(IProgressMonitor monitor) {	
-		String[] gitStrcuture = {".git", ".git/objects", ".git/refs", 
-				".git/objects/info", ".git/objects/pack", ".git/refs/heads", ".git/refs/tags"};
-		createFolders(gitStrcuture, monitor);
-		createFile("HEAD", ".git/", "ref: refs/heads/master\n", monitor);
-		createFile(".gitignore", "", "temp\n", monitor);
+	private void createLocalGitRepo(IProgressMonitor monitor) throws IOException {	
+		File directory = createGitDirectory(project.getName());		
+		InitCommand command = new InitCommand();
+		command.setDirectory(directory);
+		command.setBare(false);
+		Repository repository = command.call().getRepository();
+		addRepoToClose(repository);
+				
+//		String[] gitStrcuture = {".git", ".git/objects", ".git/refs", 
+//				".git/objects/info", ".git/objects/pack", ".git/refs/heads", ".git/refs/tags"};
+//		createFolders(gitStrcuture, monitor);
+//		createFile("HEAD", ".git/", "ref: refs/heads/master\n", monitor);
+//		createFile(".gitignore", "", "temp\n", monitor);
+	}
+	
+	/**
+	 * Creates a unique directory for a test
+	 *
+	 * @param name
+	 *            a subdirectory
+	 * @return a unique directory for a test
+	 * @throws IOException
+	 */
+	protected File createGitDirectory(String name) throws IOException {
+		
+		String gitdirName = root.getLocation().toString() + "/" + name;
+		
+		File directory = new File(gitdirName);
+		FileUtils.mkdirs(directory);
+		return directory.getCanonicalFile();
+	}
+	
+	public void addRepoToClose(Repository r) {
+		toClose.add(r);
 	}
 	
 	/**
