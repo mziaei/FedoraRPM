59b264374c89500fccb56367002d4be06c8aa658
Minoo Ziaei <mziaei@redhat.com>
2011-08-02 17:33:15 -0400

    forgot to add pageFour

---
+++ org.fedoraproject.eclipse.packager.fedorarpm/src/org/fedoraproject/eclipse/packager/fedorarpm/internal/ui/FedoraRPMProjectPageFour.java
@@ -0,0 +1,35 @@
+package org.fedoraproject.eclipse.packager.fedorarpm.internal.ui;
+
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.linuxtools.rpm.ui.editor.wizards.SpecfileNewWizardPage;
+import org.eclipse.swt.widgets.Composite;
+import org.fedoraproject.eclipse.packager.fedorarpm.FedoraRPMText;
+import org.fedoraproject.eclipse.packager.fedorarpm.FedorarpmPlugin;
+
+public class FedoraRPMProjectPageFour extends SpecfileNewWizardPage {
+
+
+	/**
+	 * Create the wizard.
+	 */
+	public FedoraRPMProjectPageFour(String pageName, ISelection selection) {
+		super(selection);
+		setTitle(FedoraRPMText.FedoraRPMProject_title);
+		setDescription(FedoraRPMText.FedoraRPMProject_description);
+		FedorarpmPlugin
+				.getImageDescriptor(FedoraRPMText.FedoraRPMProject_image);
+
+	}
+
+	/**
+	 * Create contents of the wizard.
+	 *
+	 * @param parent
+	 */
+	@Override
+	public void createControl(Composite parent) {
+		super.createControl(parent);
+		setPageComplete(true);
+	}
+
+}
