6c8fa17cf8d9865249b461b94a852074ffdc9587
Minoo Ziaei <mziaei@redhat.com>
2011-08-09 16:20:33 -0400

    removed linuxtools

--- profiling/org.eclipse.linuxtools.profiling.launch/src/org/eclipse/linuxtools/profiling/launch/ProfileLaunchConfigurationTab.java
+++
@@ -1,7 +0,0 @@
-package org.eclipse.linuxtools.profiling.launch;
-
-import org.eclipse.debug.ui.AbstractLaunchConfigurationTab;
-
-public abstract class ProfileLaunchConfigurationTab extends AbstractLaunchConfigurationTab{
-
-}
