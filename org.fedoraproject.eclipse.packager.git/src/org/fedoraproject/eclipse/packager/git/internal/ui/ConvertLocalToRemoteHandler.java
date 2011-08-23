ab3dd8b985a67fd6815d0fed8012b0778efe4d16
Minoo Ziaei <mziaei@redhat.com>
2011-08-23 16:56:16 -0400

    moved convert local to remote -> to .git

--- org.fedoraproject.eclipse.packager.git/src/org/fedoraproject/eclipse/packager/git/internal/ui/ConvertLocalToRemoteHandler.java
+++ org.fedoraproject.eclipse.packager.git/src/org/fedoraproject/eclipse/packager/git/internal/ui/ConvertLocalToRemoteHandler.java
@@ -8,7 +8,7 @@
  * Contributors:
  *     Red Hat Inc. - initial API and implementation
  *******************************************************************************/
-package org.fedoraproject.eclipse.packager.internal.handlers;
+package org.fedoraproject.eclipse.packager.git.internal.ui;
 
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
